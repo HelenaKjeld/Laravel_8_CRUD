99aa53fb7d9e2b38e53908133fdd09d3d5a27259
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2026-02-27 14:07:18 +0100

    Add product management views and routes
    - Created a layout for product views in `resources/views/products/layout.blade.php`.
    - Added a form for creating new products in `resources/views/products/create.blade.php`.
    - Established routes for product management in `routes/web.php`.
    - Implemented a basic API route in `routes/api.php`.
    - Set up broadcasting channels in `routes/channels.php`.
    - Defined console commands in `routes/console.php`.
    - Added unit and feature tests in `tests/Unit` and `tests/Feature`.
    - Configured asset management with Laravel Mix in `webpack.mix.js`.
    - Included necessary .gitignore files for storage and framework directories.
    - Updated the welcome view in `resources/views/welcome.blade.php`.

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 8 CRUD</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <h1>Laravel 8 CRUD</h1>
+
+    <div class="container">
+        @yield('content')
+    </div>
+    
+</body>
+</html>
